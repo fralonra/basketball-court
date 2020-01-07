b369ffd218ec913de6f099ca3d05c003af04d812
fralonra <fralonra@aliyun.com>
2020-01-08 01:13:13 +0800

    chore: update package.json. rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,31 @@
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 import { terser } from 'rollup-plugin-terser'
 import filesize from 'rollup-plugin-filesize'
 
 export default [{
   input: 'src/index.js',
-  plugins: [filesize()],
+  plugins: [
+    commonjs(),
+    json(),
+    filesize()
+  ],
   output: {
     file: 'dist/basketball-court.js',
     format: 'umd',
-    name: 'Court'
+    name: 'court'
   }
 }, {
   input: 'src/index.js',
-  plugins: [terser(), filesize()],
+  plugins: [
+    commonjs(),
+    json(),
+    terser(),
+    filesize()
+  ],
   output: {
     file: 'dist/basketball-court.min.js',
     format: 'umd',
-    name: 'Court'
+    name: 'court'
   }
 }]
