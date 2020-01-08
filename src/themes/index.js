52eacb0432b5f2075b8437602ceaf40f640d2b89
fralonra <fralonra@aliyun.com>
2020-01-08 12:28:06 +0800

    add preset themes: beach/steppe/volcano

--- src/themes/index.js
+++ src/themes/index.js
@@ -5,6 +5,57 @@ const plain = {
   }
 }
 
+const beach = {
+  global: {
+    fill: 'transparent',
+    stroke: '#fff'
+  },
+  court: {
+    fill: '#54A2C2'
+  },
+  centerCircle: {
+    fill: '#CFA246'
+  },
+  restrainCircle: false,
+  lane: {
+    fill: '#CFA246'
+  },
+  innerLane: false
+}
+
+const steppe = {
+  global: {
+    fill: 'transparent',
+    stroke: '#fff'
+  },
+  court: {
+    fill: '#4C6649'
+  },
+  restrainCircle: false,
+  innerLane: false,
+}
+
+const volcano = {
+  global: {
+    fill: 'transparent',
+    stroke: '#fff'
+  },
+  court: {
+    fill: '#286B5B'
+  },
+  centerCircle: {
+    fill: '#A35A5C'
+  },
+  restrainCircle: false,
+  lane: {
+    fill: '#A35A5C'
+  },
+  innerLane: false
+}
+
 module.exports = {
-  plain
+  plain,
+  beach,
+  steppe,
+  volcano
 }
